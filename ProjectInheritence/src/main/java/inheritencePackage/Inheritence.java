a711bc2cfbe3573fc4eab74840602eb15f6a5008
HP <HP@DESKTOP-UC8GQ4P>
2023-09-13 07:34:55 +0530

    My third commit

---
+++ ProjectInheritence/src/main/java/inheritencePackage/Inheritence.java
@@ -0,0 +1,11 @@
+package inheritencePackage;
+
+public class Inheritence {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hiii");
+
+	}
+
+}
---
+++ ProjectInheritence/src/main/java/inheritencePackage/Inheritence1.java
@@ -0,0 +1,11 @@
+package inheritencePackage;
+
+public class Inheritence1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Helloo");
+
+	}
+
+}
