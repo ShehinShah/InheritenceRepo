0f558fbe86772094e9e981f11b0723aba9398ecf
HP <HP@DESKTOP-UC8GQ4P>
2023-09-13 07:50:57 +0530

    Fourth commit

---
+++ ProjectInheritence/src/main/java/inheritencePackage/Inheritence3.java
@@ -0,0 +1,12 @@
+package inheritencePackage;
+
+public class Inheritence3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Morining");
+
+	}
+
+}
